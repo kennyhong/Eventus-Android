568bcdc09e27f7fd1baa8af3a84d15f2e14556ae
fehre3 <fehre3@myumanitoba.ca>
2017-02-13 21:44:11 -0600

    Added a button and text field. Next they need to navigate to the new activity.

--- app/src/main/java/com/example/baronvonfaustiii/eventus_android/MainActivity.java
+++ app/src/main/java/com/example/baronvonfaustiii/eventus_android/MainActivity.java
@@ -2,12 +2,59 @@ package com.example.baronvonfaustiii.eventus_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
+//test
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupListeners();
+
     }
+
+    public void setupListeners()
+    {
+        Button signIn = (Button)findViewById( R.id.OK_Button);
+
+
+        signIn.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                // Do something in response to button click
+                EditText userNameField = (EditText)findViewById((R.id.UsernameField));
+                userNameField.setText("Signing you in");
+
+
+            }
+        });
+
+        final EditText userNameField = (EditText)findViewById((R.id.UsernameField));
+
+        userNameField.setOnKeyListener(new View.OnKeyListener()
+        {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER))
+                { 
+                    // Perform action on key press
+                   userNameField.setText("Signing you in");
+
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
 }
