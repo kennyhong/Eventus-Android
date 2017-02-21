995400ad7c0cca6d4a65b486c3336122d1bfa40e
fehre3 <fehre3@myumanitoba.ca>
2017-02-21 14:21:52 -0600

    Added test suite stub file. Run tests by: right click on "runUnitTests" file, and select run, runUnitTests. Will test all tests.

---
+++ app/src/test/java/com/example/baronvonfaustiii/eventus_android/RunUnitTests.java
@@ -0,0 +1,40 @@
+package com.example.baronvonfaustiii.eventus_android;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class RunUnitTests extends TestSuite
+{
+    public static TestSuite suite;
+
+    public static Test suite()
+    {
+        suite = new TestSuite("All tests");
+        testObjects();
+        testBusiness();
+        //testPersistence(); Not fully functional
+        return suite;
+    }
+
+    @org.junit.Test
+    private static void testObjects()
+    {
+        //suite.addTestSuite(GameTest.class);
+        System.out.println("Starting Object tests");
+        suite.addTestSuite(ExampleUnitTest.class);
+        System.out.println("Finished Object tests");
+
+    }
+
+    private static void testBusiness()
+    {
+        //suite.addTestSuite(InteractionsTest.class);
+
+    }
+
+    private static void testPersistence()
+    {
+       // suite.addTestSuite(DBTest.class);
+    }
+
+}
