5b7c53997f3c4a120e34f165fc7ef38a59e875b1
fehre3 <fehre3@myumanitoba.ca>
2017-02-20 23:26:27 -0600

    Can add and remove mock service labels from event page. A few edge cases are prevented. Still require testing.

--- app/src/main/java/com/example/baronvonfaustiii/eventus_android/ViewEventActivity.java
+++ app/src/main/java/com/example/baronvonfaustiii/eventus_android/ViewEventActivity.java
@@ -8,16 +8,20 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import org.w3c.dom.Text;
 
 public class ViewEventActivity extends AppCompatActivity
 {
+    boolean editMode = false;
+    boolean removeServiceMode = false;
+    LinearLayout scrollLayout = null ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_event);
-
+        scrollLayout = (LinearLayout) findViewById(R.id.ServiceScrollLinearLayout);
         setupListeners();
         populateEventFields();
     }
@@ -46,29 +50,109 @@ public class ViewEventActivity extends AppCompatActivity
                 // Begin new Dialog actions for adding a new event
 
                 // For now, simply add another button to the view
+                turnOffRemoveServiceMode();
+               // TextView newServiceButton =
+                createNewServiceTextView();
 
-                TextView newServiceButton = createNewServiceTextView();
+                //LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 
-                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-
-                LinearLayout scrollLayout = (LinearLayout) findViewById(R.id.ServiceScrollLinearLayout);  // mainEventList.findViewById(R.id.LinearScrollLayout).ad .addView(newEventButton); // takes a new view as a parameter
-                scrollLayout.addView(newServiceButton, lp);
+                //LinearLayout scrollLayout = (LinearLayout) findViewById(R.id.ServiceScrollLinearLayout);  // mainEventList.findViewById(R.id.LinearScrollLayout).ad .addView(newEventButton); // takes a new view as a parameter
+                //scrollLayout.addView(newServiceButton, lp);
             }
             });
+
+        ImageButton removeServiceButton = (ImageButton)findViewById((R.id.removeServiceButton));
+
+        removeServiceButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                // Do something in response to button click
+
+                //LinearLayout scrollLayout = (LinearLayout) findViewById(R.id.ServiceScrollLinearLayout);
+
+                if(removeServiceMode)
+                {// then turn it off
+                    turnOffRemoveServiceMode();
+                }
+                else
+                {// turn it on
+
+                    if(scrollLayout.getChildCount() > 0)
+                    {
+                        removeServiceMode = true;
+                        for(int i = 0 ; i < scrollLayout.getChildCount(); i++)
+                        {
+                            TextView temp = (TextView) scrollLayout.getChildAt(i);
+                            temp.setBackgroundColor(0xCCff0066);
+
+                        }
+                    }
+
+                }
+
+
+            }
+        });
 
 
     }
 
-    public TextView createNewServiceTextView()
+    public void turnOffRemoveServiceMode()
     {
+        //LinearLayout scrollLayout = (LinearLayout) findViewById(R.id.ServiceScrollLinearLayout);
+
+        removeServiceMode = false;
+
+        for(int i = 0 ; i < scrollLayout.getChildCount(); i++)
+        {
+            TextView temp = (TextView) scrollLayout.getChildAt(i);
+            temp.setBackgroundColor(-1);
+
+        }
+    }
+
+
+    public void createNewServiceTextView()
+    {// later this also may take parameter values from this field or elsewhere for creating the services stuff
         // later this can be used for actually assembling the service object maybe
+
+        //LinearLayout scrollLayout = (LinearLayout) findViewById(R.id.ServiceScrollLinearLayout);
+
         TextView result = new TextView(this);
         result.setText("New Service Added");
         result.setBackgroundColor(-1);
         result.setTextSize(24f);
         result.setTextColor((0xff000000));
-        return result;
+        result.setClickable(true);
+
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        scrollLayout.addView(result, lp);
+
+        result.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                // Do something in response to button click
+
+
+                // make sure that there are elements to remove
+
+                if(removeServiceMode)
+                {// then remove this service,
+                    // for now just delete the item, later, add a confirm dialog etc.
+                    scrollLayout.removeView(v);
+                    turnOffRemoveServiceMode();
+                }
+                else
+                {// turn it on
+                    // do nothing for now, later view details of that service
+
+                }
+
+
+            }
+        });
+
+        //return result;
     }
+
 
     public void populateEventFields()
     {
