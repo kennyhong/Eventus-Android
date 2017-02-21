3d56b0568519a0f4452df2be23e74c14e42fe96d
fehre3 <fehre3@myumanitoba.ca>
2017-02-21 14:49:19 -0600

    Missed this with the last commit. Need to add stub event data, then add some tests.

---
+++ app/src/test/java/com/example/baronvonfaustiii/eventus_android/CountServicesTest.java
@@ -0,0 +1,24 @@
+package com.example.baronvonfaustiii.eventus_android;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+
+public class CountServicesTest extends TestCase
+{
+    public CountServicesTest(String arg0)
+    {
+        super(arg0);
+    }
+
+    @Test
+    public void testSetup()
+    {
+
+        // load up a stub instance of an event activity, then test adding and removing
+        // elements from the services list
+
+        
+
+    }
+}
